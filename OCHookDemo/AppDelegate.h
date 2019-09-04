8a8d6b4249a4c2b9db15e01a286e1f1b489a981c
tony0x59 <hanningkong@gmail.com>
2019-09-04 21:48:47 +0800

    Initial Commit

---
+++ OCHookDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCHookDemo
+//
+//  Created by Tony on 2019/9/4.
+//  Copyright © 2019 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
